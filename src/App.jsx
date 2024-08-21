5fc27e7311d31eb485383b3191f80195c669708e
LinLehShweYieNyein <u6520274@au.edu>
2024-08-21 07:47:41 +0700

    I

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <Router>
+      <Router basename='car-analytics'>
         <AppNavbar />
         <Routes>
           <Route path="/" element={<Dashboard />} />
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { HashRouter } from 'react-router-dom';
+
 createRoot(document.getElementById('root')).render(
-    <HashRouter>
-      <App />
-    </HashRouter>
+  <StrictMode>
+    <App />
+  </StrictMode>
 
 )
